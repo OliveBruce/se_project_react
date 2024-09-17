8376635a4395535ba828d47fabe4e67192f9baa6
Olivia Bruce <olivia.bruce1995@gmail.com>
2024-09-16 23:24:04 -0500

    Add functionality to modals. Add API functionality to weather card and temp.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright-name">Developed by Name Surname</p>
+      <p className="footer__copyright-year">2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
